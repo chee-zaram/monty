6c942ff5dda93d0e1df189d560d6f25e4b6c69bd
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-12-15 16:45:11 +0100

    fix: Pass appropriate argument to FREE_STACK()
    This was necessary after the function changed to taking the address of
    the pointer to the node.
    
    BREAKING CHANGE
    Before change, function took in a pointer to a node.

--- opcodes_arthmtcs.c
+++ opcodes_arthmtcs.c
@@ -12,7 +12,7 @@
 
 ADD {
 	if (!*stack || !(*stack)->next)
-		FREE_STACK(*stack), CLOSE(ERR_ADD, line_number);
+		FREE_STACK(stack), CLOSE(ERR_ADD, line_number);
 
 	(*stack)->next->n = (*stack)->n + (*stack)->next->n;
 	pop(stack, 0);
@@ -20,7 +20,7 @@ ADD {
 
 SUB {
 	if (!*stack || !(*stack)->next)
-		FREE_STACK(*stack), CLOSE(ERR_SUB, line_number);
+		FREE_STACK(stack), CLOSE(ERR_SUB, line_number);
 
 	(*stack)->next->n = (*stack)->next->n - (*stack)->n;
 	pop(stack, 0);
@@ -28,11 +28,11 @@ SUB {
 
 DIV {
 	if (!*stack || !(*stack)->next)
-		FREE_STACK(*stack), CLOSE(ERR_DIV, line_number);
+		FREE_STACK(stack), CLOSE(ERR_DIV, line_number);
 
 	/* if zero division error, print to stderr and exit */
 	if ((*stack)->n == 0)
-		FREE_STACK(*stack), CLOSE(ERR_DIV_ZERO, line_number);
+		FREE_STACK(stack), CLOSE(ERR_DIV_ZERO, line_number);
 
 	(*stack)->next->n = (*stack)->next->n / (*stack)->n;
 	pop(stack, 0);
@@ -40,7 +40,7 @@ DIV {
 
 MUL {
 	if (!*stack || !(*stack)->next)
-		FREE_STACK(*stack), CLOSE(ERR_MUL, line_number);
+		FREE_STACK(stack), CLOSE(ERR_MUL, line_number);
 
 	(*stack)->next->n = (*stack)->next->n * (*stack)->n;
 	pop(stack, 0);
@@ -48,11 +48,11 @@ MUL {
 
 MOD {
 	if (!*stack || !(*stack)->next)
-		FREE_STACK(*stack), CLOSE(ERR_MOD, line_number);
+		FREE_STACK(stack), CLOSE(ERR_MOD, line_number);
 
 	/* if zero division error, print to stderr and exit */
 	if ((*stack)->n == 0)
-		FREE_STACK(*stack), CLOSE(ERR_MOD_ZERO, line_number);
+		FREE_STACK(stack), CLOSE(ERR_MOD_ZERO, line_number);
 
 	(*stack)->next->n = (*stack)->next->n % (*stack)->n;
 	pop(stack, 0);
